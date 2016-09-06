d2de37d4707b32e13fbde925001bba8703741378
DevolperPHP <devolperphp@gmail.com>
2016-09-06 04:29:55 +0300

    Update Main.php

--- src/kit/Main.php
+++ src/kit/Main.php
@@ -10,23 +10,23 @@ use pocketmine\inventory\InventoryBase;
 use pocketmine\item\Item;
 use pocketmine\permission\Permission;
 
-public function onEnabled(){
-$this->getServer()->getLogger()->info(Color::GREEN."[kit] Plugin Has Been Enabled");
+ public function onEnabled(){
+  $this->getServer()->getLogger()->info(Color::GREEN."[kit] Plugin Has Been Enabled");
 }
-public function onDisable(){
-$this->getServer()->getLogger()->info(Color::RED."[kit] Plugin Has Been Disabled");
+ public function onDisable(){
+  $this->getServer()->getLogger()->info(Color::RED."[kit] Plugin Has Been Disabled");
 }
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-switch($cmd->getName()){
-case 'kit':
-$sender->sendMessage(Color::GREEN."You Have A Player Kit");
-$sender->getInventory()->setHelmet(item::get(314, 0, 1));
-$sender->getInventory()->setChestplate(item::get(315, 0, 1));
-$sender->getInventory()->setLeggings(item::get(316, 0, 1));
-$sender->getInventory()->setBoots(item::get(317, 0, 1));
-$sender->getInventory()->additem(item::get(272, 0, 1));
-$sender->getInventory()->additem(item::get(apple, 0, 64));
-}
-}
-}
+ public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+  switch($cmd->getName()){
+    case 'kit':
+      $sender->sendMessage(Color::GREEN."You Have A Player Kit");
+      $sender->getInventory()->setHelmet(item::get(314, 0, 1));
+      $sender->getInventory()->setChestplate(item::get(315, 0, 1));
+      $sender->getInventory()->setLeggings(item::get(316, 0, 1));
+      $sender->getInventory()->setBoots(item::get(317, 0, 1));
+      $sender->getInventory()->additem(item::get(272, 0, 1));
+      $sender->getInventory()->additem(item::get(apple, 0, 64));
+     }
+   }
+ }
 }
